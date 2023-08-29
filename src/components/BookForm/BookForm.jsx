7111810571bb74e7cabe45bea7fc6e9d182dc1b1
Andrewchik <andriy.goryachiy16@gmail.com>
2023-08-29 23:42:24 +0300

    [ADD] book components

---
+++ src/components/BookForm/BookForm.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+import './BookForm.scss'
+
+function BookForm() {
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        // Make an API call to add the book
+        axios.post('/api/books', { title, author })
+            .then(response => {
+                console.log('Book added:', response.data);
+                // Clear form inputs
+                setTitle('');
+                setAuthor('');
+            })
+            .catch(error => console.error('Error adding book:', error));
+    };
+
+    return (
+        <div className="book-form">
+            <h2>Add Book</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Title"
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                    required
+                />
+                <input
+                    type="text"
+                    placeholder="Author"
+                    value={author}
+                    onChange={e => setAuthor(e.target.value)}
+                    required
+                />
+                <button type="submit">Add Book</button>
+            </form>
+        </div>
+    );
+}
+
+export default BookForm;
---
+++ src/components/BookList/BookList.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './BookList.scss'
+
+function BookList() {
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        axios.get('/api/books')
+            .then(response => setBooks(response.data))
+            .catch(error => console.error('Error fetching books:', error));
+    }, []);
+
+    return (
+        <div className="book-list">
+            <h2>Book List</h2>
+            <ul>
+                {books.map(book => (
+                    <li key={book.id}>
+                        {book.title} by {book.author}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default BookList;
